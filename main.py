1f328f96f737fab4286a28dfe94d24684d8782fd
Adrian Chong <adrian.shun1387@outlook.com>
2025-03-16 19:57:13 +1030

    function to return text from frank book

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath):
+    #book = input("Book title: ")
+    #pwd = filepath + book
+    with open(filepath) as f:
+        book_contents = f.read()
+    return book_contents
+
+def main():
+    book_texts = get_book_text("books/frankenstein.txt")
+    print(book_texts)
+
+main() 
